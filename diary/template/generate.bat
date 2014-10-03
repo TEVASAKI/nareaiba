19123051a7e6e4f7e26b91faca6b0ac334811a3d
TEVA <vanquish.ld+github@gmail.com>
2014-10-04 03:33:40 +0900

    testing#1

--- diary/template/generate.bat
+++ diary/template/generate.bat
@@ -8,4 +8,4 @@ rem example : PROMPT$  generate.bat 20141213 12月13日の徒然
 
 rem 要するに、1ページで一日単位。
 
-pandoc.exe -f markdown --template=./html5_template %1.md -o %1.html -V pagetitle="%2"
+pandoc.exe -f markdown --template=..\template\html5_template.html %1.md -o %1.html -V pagetitle="%2"
