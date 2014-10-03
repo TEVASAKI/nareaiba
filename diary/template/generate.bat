5c25000d1a6cb2c127c84a7855aa64bb6c9e3b7d
TEVA <vanquish.ld+github@gmail.com>
2014-10-04 03:49:47 +0900

    generate BAT COMPLETION!
    Caution , this scripts exec Relative PATH.

--- diary/template/generate.bat
+++ diary/template/generate.bat
@@ -4,7 +4,8 @@ rem %1 : INfile and OUTfile NAME @ Markdown (*.md) to HTML (*.html)
 rem %2 : PageTitle
 
 rem USAGE : PROMPT$  generate.bat %1 %2
-rem example : PROMPT$  generate.bat 20141213 12月13日の徒然
+rem example : PROMPT$  ../template/generate.bat 20141213 12月13日の徒然
+rem 実行場所が各ディレクトリからなので、相対パス指定で。
 
 rem 要するに、1ページで一日単位。
 
