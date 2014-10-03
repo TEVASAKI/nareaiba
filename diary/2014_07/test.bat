4e6679b5b202cbcb2d22a391c2e9c7f5fb05cb61
TEVA <vanquish.ld+github@gmail.com>
2014-10-03 18:48:29 +0900

    fenc=cp932

--- diary/2014_07/test.bat
+++ diary/2014_07/test.bat
@@ -3,4 +3,4 @@
 rem echo %1 %2 %3
 rem pandoc.exe -f markdown --template=../template/html5_template.html %1.md -o %2.html -V pagetitle="%3" -V date-meta="$(date)"
 
-start "" pandoc.exe -V date-meta=$(%~4) -f markdown --template=../template/html5_template %1.md -o %2.html -V pagetitle="%3"
+pandoc.exe -V date-meta=$(date) -f markdown --template=../template/html5_template %1.md -o %2.html -V pagetitle="%3"
