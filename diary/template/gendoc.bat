477ac340b5cac16a636bb83948ee207d3a64623e
TEVA <vanquish.ld+github@gmail.com>
2014-11-09 23:40:20 +0900

    modified:   ../template/gendoc.bat modified:   ../template/html5_template.html

--- diary/template/gendoc.bat
+++ diary/template/gendoc.bat
@@ -13,10 +13,11 @@ rem               : ~x          拡張子
 rem               : ~t          ファイルの日付と時刻
 
 rem pandoc option : --template  テンプレート指定。このシェルと同じ場所に置いておく必要がある
+rem               : -c          CSS の指定
 rem               : -f          入力形式指定
 rem               : -o          出力名指定 , 第二引数に入力ファイル名
-rem               : -V          テンプレート内のHaskell 引数指定
-rem               : -c          CSS の指定
+rem               : -S          Typography的に正しく出力する(smartMode)
+rem               : -V          テンプレート内の各種Haskell 引数指定
 
 rem SendTo "%APPDATA%\Microsoft\Windows\SendTo\gendoc.lnk"
 
@@ -31,7 +32,7 @@ if exist "%~d1%~p1%~n1%~x1" (
   goto :EOF
 )
 
-pandoc --template "%~d0%~p0\html5_template.html" -c "../template/markdown-style.css" -f markdown_github+footnotes+definition_lists+pandoc_title_block+header_attributes -o "%F%.html" "%F%%~x1"  -V pagetitle="%~n1" -V date-meta="%~t1" -V -S
+pandoc --template "%~d0%~p0\html5_template.html" -c "../template/markdown-style.css" -f markdown_github+footnotes+definition_lists+pandoc_title_block+header_attributes -o "%F%.html" "%F%%~x1" -S -V pagetitle="%~n1" -V date-meta="%~t1"
 if errorlevel 1 (
   echo pandocの処理に失敗しました
   pause
